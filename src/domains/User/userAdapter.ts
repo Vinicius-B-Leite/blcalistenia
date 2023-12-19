6c0ec5e49ddfc6ac2839a812762293fab01982e5
Vinicius Leite <viniciusleite@brq.com>
2023-12-19 20:23:50 -0300

    bufixes: desmarcando serie ao clicar na checkbox ja marcada

--- src/domains/User/userAdapter.ts
+++ src/domains/User/userAdapter.ts
@@ -3,9 +3,9 @@ import uuid from 'react-native-uuid';
 
 function adapter(user: any): UserType {
   return {
-    avatar: user.photoURI || '',
-    uid: user._id || uuid.v4().toString(),
-    username: user.username || 'Desconhecido',
+    avatar: user?.photoURI || '',
+    uid: user?._id || uuid.v4().toString(),
+    username: user?.username || 'Desconhecido',
   };
 }
 
