bb91ede6f7b15d8a04f14fe0beb05b78e34e1275
Vinicius Leite <viniciusleite@brq.com>
2024-01-24 22:54:24 -0300

    test: utils hexToRgb

---
+++ src/utils/__tests__/hexToRgb.test.ts
@@ -0,0 +1,28 @@
+import {hexToRgb} from '../hexToRgb';
+
+describe('hexToRgb', () => {
+  it('should convert hex color to rgba correctly', () => {
+    const result = hexToRgb('#ffffff', 1);
+    expect(result).toBe('rgba(255, 255, 255, 1)');
+  });
+
+  it('should not permit hex color without hash symbol', () => {
+    const result = hexToRgb('000000', 0.5);
+    expect(result).toBe('');
+  });
+
+  it('should handle uppercase hex color', () => {
+    const result = hexToRgb('#ABCDEF', 0.8);
+    expect(result).toBe('rgba(171, 205, 239, 0.8)');
+  });
+
+  it('should handle invalid hex color', () => {
+    const result = hexToRgb('zzzzzz', 1);
+    expect(result).toBe('');
+  });
+
+  it('should handle opacity correctly', () => {
+    const result = hexToRgb('#123456', 0.3);
+    expect(result).toBe('rgba(18, 52, 86, 0.3)');
+  });
+});
--- src/utils/hexToRgb.ts
+++ src/utils/hexToRgb.ts
@@ -1,12 +1,14 @@
 export function hexToRgb(hex: string, opacity: number) {
-  // Remove o caractere #, se presente
+  const isHexColor = /^#([A-Fa-f0-9]{6}|[A-Fa-f0-9]{3})$/.test(hex);
+  if (!isHexColor) {
+    return '';
+  }
+
   hex = hex.replace(/^#/, '');
 
-  // Converte para valores decimais
   var r = parseInt(hex.substring(0, 2), 16);
   var g = parseInt(hex.substring(2, 4), 16);
   var b = parseInt(hex.substring(4, 6), 16);
 
-  // Retorna como uma string no formato "rgb(r, g, b)"
   return 'rgba(' + r + ', ' + g + ', ' + b + ', ' + opacity + ')';
 }
